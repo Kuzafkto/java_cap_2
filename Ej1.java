3e315625b969da6f4be60fe7e77460b2d3641230
KuzaFkto <kuzafkto@gmail.com>
2022-10-06 08:45:45 +0200

    espaciado

--- Ej1.java
+++ Ej1.java
@@ -6,11 +6,11 @@ public class Ej1 {
         x=144;
         y=999;
 
-        System.out.println("x =" + x);
-        System.out.println("y =" + y);
-        System.out.println("x + y =" + (x + y));
-        System.out.println("x - y =" + (x - y));
-        System.out.println("x * y =" + (x * y));
-        System.out.println("x / y =" + (x / y));
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x / y = " + (x / y));
     }
 }
